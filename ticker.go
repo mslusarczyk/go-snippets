0e66cd0fe4da79518e67cc4c8b5df78abcde6652
Michal Slusarczyk <m.slusarczyk@smartrecruiters.com>
2018-03-29 14:39:51 +0200

    Timeout fix

--- ticker.go
+++ ticker.go
@@ -5,17 +5,18 @@ import (
 )
 
 const (
-	interval = 5 * time.Second
-	timeout  = 5 * time.Minute
+	interval = 2 * time.Second
+	timeout  = 15 * time.Second
 )
 
 func main() {
 	ticker := time.NewTicker(interval)
+	timeoutCh := time.After(timeout)
 	for {
 		select {
 		case <-ticker.C:
 			// do stuff
-		case <-time.After(timeout):
+		case <-timeoutCh:
 			ticker.Stop()
 			// break/return/os.exit
 		}
